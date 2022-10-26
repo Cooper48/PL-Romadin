a003e6aa60491350f31abf503485af1b45232d9f
Cooper48 <113463556+Cooper48@users.noreply.github.com>
2022-10-26 16:01:43 +0300

    Add files via upload

---
+++ Pr8-4v-2n.py
@@ -0,0 +1,14 @@
+import random
+
+def printmatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:4d}".format(matrix[i][j]), end="")
+        print()
+
+matrix = [[random.randint(-10,10) for i in range(3)] for i in range(3)]
+print("Исходная матрица:", printmatrix(matrix))
+
+matrix = [[1 if j>0 else 0 for j in i] for i in matrix]
+for i in range(len(matrix)):
+    print(*[matrix[i][j] if j<=i else '' for j in range(len(matrix[i]))],'')
