02f94d88e6352132204cf1cffac7243439234dca
Cooper48 <113463556+Cooper48@users.noreply.github.com>
2022-10-26 16:02:09 +0300

    Add files via upload

---
+++ Pr8-4v-1n.py
@@ -0,0 +1,22 @@
+import random
+
+def printmatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:4d}".format(matrix[i][j]), end="")
+        print()
+
+
+matrix = [[random.randint(-10, 10) for i in range(4)] for i in range(3)]
+print('Исходная матрица:',matrix)
+print(printmatrix(matrix))
+s=[]
+for i in range(len(matrix)):
+    s.append(sum(matrix[i]))
+print("Строка с наибольшей суммой:",matrix[s.index(max(s))],"Сумма элементов:",max(s), ";","Строка с наименьшей суммой:",matrix[s.index(min(s))],"Сумма элементов:",min(s))
+
+def printmatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:4d}".format(matrix[i][j]), end="")
+        print()
