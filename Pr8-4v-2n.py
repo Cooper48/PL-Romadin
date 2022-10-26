9e04a9bbf8854930fa22b58413063b73c92a803c
Cooper48 <113463556+Cooper48@users.noreply.github.com>
2022-10-26 16:14:32 +0300

    Update Pr8-4v-2n.py

--- Pr8-4v-2n.py
+++ Pr8-4v-2n.py
@@ -6,9 +6,9 @@ def printmatrix(matrix):
             print("{:4d}".format(matrix[i][j]), end="")
         print()
 
-matrix = [[random.randint(-10,10) for i in range(3)] for i in range(3)]
+matrix = [[random.randint(-10,10) for i in range(3)] for j in range(3)]
 print("Исходная матрица:", printmatrix(matrix))
 
-matrix = [[1 if j>0 else 0 for j in i] for i in matrix]
+matrix = [[1 if j>0 else 0 for j in i] for j in matrix]
 for i in range(len(matrix)):
     print(*[matrix[i][j] if j<=i else '' for j in range(len(matrix[i]))],'')
